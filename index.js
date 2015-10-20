467c98e3181f5c157bcfa6e49f5dd8edabe42010
Viktor Linder <viktor.linder@sbsradio.se>
2015-10-20 19:26:55 +0200

    Added Sync variants of functions.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var exec = require('child_process').exec
+var execSync = require('child_process').execSync;
 
 function _command (cmd, dir, cb) {
   if (typeof dir === 'function') cb = dir, dir = __dirname
@@ -8,6 +9,16 @@ function _command (cmd, dir, cb) {
     }
     cb(null, stdout.split('\n').join(''))
   })
+}
+
+function _commandSync(cmd, dir) {
+  if(typeof dir === 'undefined') dir = __dirname;
+  try {
+    return execSync(cmd, {cwd: dir, encoding: 'utf8'}).split('\n').join('');
+  }
+  catch(e) {
+    return '';
+  }
 }
 
 module.exports = {
@@ -23,5 +34,20 @@ module.exports = {
         str = str.substr(0, str.length-1)
         cb(null, JSON.parse('[' + str + ']'))
       })
+    }
+  , shortSync : _commandSync.bind(null, 'git rev-parse --short HEAD')
+  , longSync : _commandSync.bind(null, 'git rev-parse HEAD')
+  , branchSync : _commandSync.bind(null, 'git rev-parse --abbrev-ref HEAD')
+  , tagSync : _commandSync.bind(null, 'git describe --always --tag --abbrev=0')
+  , describeSync : _commandSync.bind(null, 'git describe --tags --always')
+  , logSync: function(dir, cb) {
+      if(typeof dir === 'function') cb = dir, dir = __dirname;
+      _commandSync('git log --no-color --pretty=format:\'[ "%H", "%s", "%cr", "%an" ],\' --abbrev-commit',
+          dir,
+          function(err, str) {
+              if(err) return cb(err);
+              str = str.substr(0, str.length - 1);
+              cb(null, JSON.parse('[' + str + ']'));
+          })
     }
 }
